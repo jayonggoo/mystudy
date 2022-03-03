9fbe30e5c7060e4da92c17e4ba9bbfd31c2a46ff
jayong.goo <jayong.goo@samsung.com>
2022-03-03 23:07:56 +0900

    2022년 3월 3일까지 FirstUniqueCharacerString 학습

---
+++ 1dayCoding/basic/Strings/FirstUniqueCharacerString.java
@@ -0,0 +1,25 @@
+public class FirstUniqueCharacerString {
+
+    public static void main(String[] args) {
+        String s = "loveleetcode";
+        System.out.println(firstUniqChar(s));
+    }
+
+    public static int firstUniqChar(String s)
+    {
+        int freq[] = new int[26];
+
+        for(int i = 0 ; i < s.length(); i++)
+        {
+            freq[s.charAt(i) - 'a']++;
+        }
+
+        for(int i = 0 ; i < s.length(); i++)
+        {
+            if (freq[s.charAt(i) - 'a'] == 1)
+                return i;
+        }
+
+        return -1;
+    }
+}
